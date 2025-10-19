4a0b4b3db8261365a1af2af1b8d6177d1d991004
DalinarKholin <osadowskikacper@gmail.com>
2025-10-19 23:47:30 +0200

    essa

---
+++ list1/client/client.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func main() {
+	// Załaduj certyfikat + klucz klienta (prezentowany do serwera)
+	clientCert, err := tls.LoadX509KeyPair("../certs/service.crt", "../certs/serviceCert.key")
+	if err != nil {
+		log.Fatal("LoadX509KeyPair:", err)
+	}
+
+	tlsCfg := &tls.Config{
+		Certificates: []tls.Certificate{clientCert},
+		MinVersion:   tls.VersionTLS12,
+	}
+
+	tr := &http.Transport{
+		TLSClientConfig: tlsCfg,
+	}
+	client := &http.Client{Transport: tr}
+
+	resp, err := client.Get("https://127.0.0.1/")
+	if err != nil {
+		log.Fatal("GET:", err)
+	}
+	defer resp.Body.Close()
+	body, _ := io.ReadAll(resp.Body)
+	fmt.Printf("Status: %s\nBody:\n%s\n", resp.Status, body)
+}
---
+++ list1/server/server.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"crypto/sha256"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/pem"
+	"errors"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"os"
+)
+
+func must(b []byte, err error) []byte {
+	if err != nil {
+		log.Fatal(err)
+	}
+	return b
+}
+
+func loadFirstCACert(pemBytes []byte) (*x509.Certificate, error) {
+	for {
+		var block *pem.Block
+		block, pemBytes = pem.Decode(pemBytes)
+		if block == nil {
+			return nil, errors.New("no CERTIFICATE block found in client-ca.crt")
+		}
+		if block.Type == "CERTIFICATE" {
+			return x509.ParseCertificate(block.Bytes)
+		}
+		// skip non-certificate blocks (e.g., PRIVATE KEY, PKCS7, etc.)
+	}
+}
+
+func main() {
+	gin.SetMode(gin.DebugMode)
+
+	r := gin.New()
+	r.Use(gin.Recovery())
+
+	r.GET("/", func(c *gin.Context) { c.JSON(http.StatusOK, gin.H{"hello": "tls"}) })
+
+	srvCert, err := tls.LoadX509KeyPair("../certs/service.crt", "../certs/serviceCert.key")
+	caPEM := must(os.ReadFile("/home/dalinarkholin/GolandProjects/BOW/list1/certs/bow.inc.ca.crt"))
+	clientCAPool := x509.NewCertPool()
+	if !clientCAPool.AppendCertsFromPEM(caPEM) {
+		log.Fatal("append client CA failed")
+	}
+	// 3) Pin CA po SPKI (opcjonalnie, ale zalecane)
+	caCert, err := loadFirstCACert(caPEM)
+	if err != nil {
+		log.Fatalf("parse client-ca: %v", err)
+	}
+	pin := sha256.Sum256(caCert.RawSubjectPublicKeyInfo)
+	log.Printf("Pinned client-CA SPKI sha256(base64): %s", base64.StdEncoding.EncodeToString(pin[:]))
+
+	tcfg := &tls.Config{
+		MinVersion:   tls.VersionTLS12,
+		Certificates: []tls.Certificate{srvCert},
+		ClientAuth:   tls.RequireAndVerifyClientCert,
+		ClientCAs:    clientCAPool,
+		VerifyConnection: func(cs tls.ConnectionState) error {
+			if len(cs.VerifiedChains) == 0 {
+				return x509.CertificateInvalidError{}
+			}
+			root := cs.VerifiedChains[0][len(cs.VerifiedChains[0])-1]
+			got := sha256.Sum256(root.RawSubjectPublicKeyInfo)
+			if got != pin {
+				return x509.UnknownAuthorityError{}
+			}
+			// (opcjonalnie) sprawdź EKU clientAuth:
+			leaf := cs.VerifiedChains[0][0]
+			okEKU := false
+			for _, eku := range leaf.ExtKeyUsage {
+				if eku == x509.ExtKeyUsageClientAuth {
+					okEKU = true
+					break
+				}
+			}
+			if !okEKU {
+				return x509.CertificateInvalidError{}
+			}
+			return nil
+		},
+	}
+
+	httpsSrv := &http.Server{
+		Addr:      ":443",
+		Handler:   r,
+		TLSConfig: tcfg,
+	}
+
+	if err := httpsSrv.ListenAndServeTLS("../certs/service.crt", "../certs/serviceCert.key"); err != nil {
+		log.Fatalf("https server: %v", err)
+	}
+}
